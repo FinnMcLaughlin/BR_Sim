ddae8e5a6088f2a83fb0c0fe8b8c54280888a45a
Finn <finnmclaughlin2@gmail.com>
2021-02-09 16:28:28 +0100

    Updated player decision making functionality to start scaling up to include other factors in the decision making process. Grid map now also tracks player locations, how many players are on a tile etc.

--- play_BR.py
+++ play_BR.py
@@ -19,15 +19,35 @@ class GridTile:
         self.x = x
         self.y = y
         self.danger_zone = False
-        self.object = tile_object.NONE
+        self.object = [tile_object.NONE]
 
     def update_tile_object(self, t_object):
-        self.object = t_object
+        print("\n-----------Previous Tile Object " + str(self.x) + ", " + str(self.y) + " -> " + str(self.object))
+        print("Adding: " + str(t_object))
+        if t_object == tile_object.NONE:
+            self.object = [tile_object.NONE]
+        else:
+            if tile_object.NONE in self.object:
+                self.object.remove(tile_object.NONE)
+            self.object.append(t_object)
+
+        print("\n-----------New Tile Object " + str(self.x) + ", " + str(self.y) + " -> " + str(self.object))
+
+    def remove_tile_object(self, t_object):
+        if t_object in self.object:
+            print("\n-----------Previous Tile Object " + str(self.x) + ", " + str(self.y) + " -> " + str(self.object))
+            print("Removing: " + str(t_object))
+            self.object.pop(self.object.index(t_object))
+            if len(self.object) < 1:
+                self.object.append(tile_object.NONE)
+            print("\n-----------New Tile Object " + str(self.x) + ", " + str(self.y) + " -> " + str(self.object))
+
 
 SMALL_MAP = 5
 MEDIUM_MAP = 11
 LARGE_MAP = 21
 
+#--Map Functionality
 def initializeMap(size):
     return populate_grid(size, size)
 
@@ -52,8 +72,9 @@ def encloseRing(_grid_map, count):
     for row in grid_map:
         for tile in row:
             if tile.x == minimum or tile.x == maximum or tile.y == minimum or tile.y == maximum:
-                tile.danger_zone = True
-                tile.update_tile_object(tile_object.DANGER_ZONE)
+                if not tile.danger_zone:
+                    tile.danger_zone = True
+                    tile.update_tile_object(tile_object.DANGER_ZONE)
 
 
 def displayMap(_grid_map):
@@ -82,13 +103,34 @@ def initializeWeapons():
 def player_turn(_player, _grid_map):
     surrounding_tiles = _player.identify_new_information(_grid_map, tile_object)
 
-    danger_tiles = player.assess_information(surrounding_tiles, tile_object)
+    danger_tiles = _player.assess_information(surrounding_tiles, tile_object)
 
     print(danger_tiles)
 
-    player.make_decision(danger_tiles)
+    decision, details =_player.make_decision(danger_tiles, tile_object)
+
+    assess_decision(_player, _grid_map, decision, details)
 
 
+def assess_decision(_player, _grid_map, _decision, _details):
+    if _decision == "move_player":
+        prev_x = _details[0][0]
+        prev_y = _details[0][1]
+        new_x = _details[1][0]
+        new_y = _details[1][1]
+
+        _grid_map[prev_x][prev_y].remove_tile_object(tile_object.PLAYER)
+        _player.move_player(new_x, new_y)
+        _grid_map[new_x][new_y].update_tile_object(tile_object.PLAYER)
+
+    if _decision == "attack_enemy":
+        print("ATTACK")
+
+    if _decision == "no_moves":
+        print("No Move")
+
+
+#--Combat Functionality
 def get_hit_limit_value(armour_value, mobility_value, attack_speed):
     print("Attacking Speed: " + str(attack_speed))
 
@@ -99,13 +141,11 @@ def get_hit_limit_value(armour_value, mobility_value, attack_speed):
     elif attack_speed < mobility_value:
         return round(((armour_value * 6) - ((armour_value * 3) * (attack_speed / mobility_value))) / 5)
 
-
 def calculate_bypass_armour(hit_limit):
     return ((20 - hit_limit) / 3) * 2
 
 def calculate_hit_damage(weapon_power_value):
     return random.randrange(int(weapon_power_value / 2), (weapon_power_value + 1))
-
 
 def simulate_combat(attacking_player, enemy_player):
     hit_limit = get_hit_limit_value(enemy_player.get_current_armour(), enemy_player.get_current_mobility(),
@@ -117,7 +157,7 @@ def simulate_combat(attacking_player, enemy_player):
 
     print("By Pass Limit Value: " + str(bypass_armour_limit))
 
-    roll_value = attacking_player.attempt_move()
+    roll_value = attacking_player.attempt_action()
 
     if roll_value == 20:
         print("!!Critical Hit!!")
@@ -151,8 +191,10 @@ if __name__ == "__main__":
     overall_count = 0
 
     player1 = Player(3, 1, 100, 10, 28, all_weapons[0])
+    grid_map[3][1].update_tile_object(tile_object.PLAYER)
     player2 = Player(3, 1, 100, 10, 22, all_weapons[1])
-
+    # grid_map[3][1].update_tile_object(tile_object.PLAYER)
+    '''
     while player1.get_current_health() > 0 and player2.get_current_health() > 0:
         simulate_combat(player1, player2)
         simulate_combat(player2, player1)
@@ -169,7 +211,7 @@ if __name__ == "__main__":
         while turn_count < 3:
             print("\nTurn: " + str(turn_count))
             time.sleep(3)
-            player_turn(player, grid_map)
+            player_turn(player1, grid_map)
             turn_count += 1
 
         encloseRing(grid_map, overall_count)
@@ -178,4 +220,4 @@ if __name__ == "__main__":
 
         overall_count += 1
 
-    displayMap(grid_map)'''
+    displayMap(grid_map)
--- player_class.py
+++ player_class.py
@@ -26,7 +26,13 @@ class Player:
     def get_current_weapon(self):
         return self.weapon
 
-
+    '''
+    # Check current tile for objects
+    # Check surrounding tile for objects
+    # If current tile contains enemy and is not next to danger zone -> combat initiated
+    # If current tile contains enemy and is next to danger zone -> move
+    # If current tile is next to danger zone -> move
+    '''
     #--Identify Methods
     def identify_new_information(self, tile_map, tile_object):
         surrounding_tiles = self.check_surroundings(tile_map, tile_object)
@@ -34,7 +40,7 @@ class Player:
 
     # Check contents of surrounding tiles
     def check_surroundings(self, tile_map, tile_object):
-        surround_tiles = []
+        surround_tiles = [tile_map[self.x][self.y].object]
 
         if not self.y - 1 < 0:
             surround_tiles.append(tile_map[self.x][self.y - 1].object)
@@ -64,7 +70,8 @@ class Player:
         surr_tile_index = 0
 
         for tile in surrounding_tiles:
-            if not tile == tile_objects.NONE:
+            print("TILE: " + str(tile))
+            if not tile_objects.NONE in tile:
                 danger_info = {surr_tile_index: tile}
                 danger_tiles.append(danger_info)
 
@@ -72,37 +79,70 @@ class Player:
 
         return danger_tiles
 
-    def make_decision(self, danger_tiles):
+    def make_decision(self, danger_tiles, t_object):
         # Index 0 - Danger Above - Move Y+1
         # Index 1 - Danger Right - Move X-1
         # Index 2 - Danger Below - Move Y-1
         # Index 3 - Danger Left - Move X+1
 
+        # return player_action: [action_details]
+
+        # If DANGER_ZONE in danger_tiles, move
+        # Elif PLAYER in danger_tiles, combat
+
+        enemy_present = False
+        enemy_index = 0
+        danger_zone_present = False
+        danger_index = 0
+
         if len(danger_tiles) > 0:
             for tile in danger_tiles:
                 for index in tile:
+
                     print(str(index) + " " + str(tile[index]))
 
-                    if index == 0:
-                        curr_x, curr_y = self.get_current_position()
-                        self.move_player(curr_x, curr_y+1)
-                        return
-                    if index == 1:
-                        curr_x, curr_y = self.get_current_position()
-                        self.move_player(curr_x-1, curr_y)
-                        return
-                    if index == 2:
-                        curr_x, curr_y = self.get_current_position()
-                        self.move_player(curr_x, curr_y-1)
-                        return
-                    if index == 3:
-                        curr_x, curr_y = self.get_current_position()
-                        self.move_player(curr_x+1, curr_y)
-                        return
+                    if t_object.DANGER_ZONE in tile[index] :
+                        danger_zone_present = True
+                        danger_index = index
+
+                    if t_object.PLAYER in tile[index]:
+                        enemy_present = True
+                        enemy_index = index
+
+
+        if danger_zone_present:
+            if danger_index == 1:
+                curr_x, curr_y = self.get_current_position()
+                return "move_player", [[curr_x, curr_y], [curr_x, curr_y+1]]
+            if danger_index == 2:
+                curr_x, curr_y = self.get_current_position()
+                return "move_player", [[curr_x, curr_y], [curr_x - 1, curr_y]]
+            if danger_index == 3:
+                curr_x, curr_y = self.get_current_position()
+                return "move_player", [[curr_x, curr_y], [curr_x, curr_y - 1]]
+            if danger_index == 4:
+                curr_x, curr_y = self.get_current_position()
+                return "move_player", [[curr_x, curr_y], [curr_x + 1, curr_y]]
+
+        '''if enemy_present:
+            if enemy_index == 0:
+                curr_x, curr_y = self.get_current_position()
+                return "attack_enemy", [[curr_x, curr_y], [curr_x, curr_y+1]]
+            if enemy_index == 1:
+                curr_x, curr_y = self.get_current_position()
+                return "attack_enemy", [[curr_x, curr_y], [curr_x - 1, curr_y]]
+            if enemy_index == 2:
+                curr_x, curr_y = self.get_current_position()
+                return "attack_enemy", [[curr_x, curr_y], [curr_x, curr_y - 1]]
+            if enemy_index == 3:
+                curr_x, curr_y = self.get_current_position()
+                return "attack_enemy", [[curr_x, curr_y], [curr_x + 1, curr_y]]'''
+
+        return "no_moves", []
 
 
     #--Attempt Methods
-    def attempt_move(self):
+    def attempt_action(self):
         return random.randrange(1, 21)
 
     def move_player(self, new_x, new_y):
